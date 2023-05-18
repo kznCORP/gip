1806d66ff461b24db6c98e00812caf70a4000408
Jericho Filllon <fillonjericho@gmail.com>
2023-05-17 23:56:27 -0700

    Added a Navbar + started implementing the expenses section of the site. Tailwind is growing on me.

--- app/page.js
+++ app/page.js
@@ -1,19 +1,53 @@
-import Head from "next/head";
+import { MdOutlineAdd } from "react-icons/md";
+
+import { currencyFormatter } from "../lib/utils";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>GIP - Group Itinerary Planner</title>
-      </Head>
+      <main className="px-4">
+        <section>
+          <h2 className="text-4xl font-bold">Expenses</h2>
+          <div className="my-5">
+            <small className="text-md text-gray-400">Total Amt. Spent</small>
+            <p>{currencyFormatter(1234.56)}</p>
+          </div>
+        </section>
 
-      <main>
-        <div className="fixed top-0 left-0 h-screen w-1/6 m-0 flex flex-col bg-slate-300 ">
-          <i>Overview</i>
-          <i>Schedule</i>
-          <i>Packing List</i>
-          <i>Expenses</i>
-        </div>
+        <section>
+          <button className="text-md my-5 flex   items-center   gap-2 rounded-lg bg-blue-600 p-3 font-medium text-white">
+            Add Expense <MdOutlineAdd className="text-xl" />
+          </button>
+        </section>
+
+        {/* List of Expenses */}
+        <section>
+          <h3 className="text-xl text-gray-400">List of expenses</h3>
+
+          {/* Expense Item */}
+          <div className="flex items-center justify-between rounded-lg bg-slate-600 px-2 py-3 text-white">
+            {/* Price */}
+            <div>
+              <p className="text-sm">{currencyFormatter(500)}</p>
+            </div>
+
+            {/* Name */}
+            <div>
+              <p className="text-sm">Marukame Udon</p>
+            </div>
+
+            {/* Categories */}
+            <div className="flex items-center gap-2">
+              <div className="h-[15px] w-[15px] rounded-full bg-red-400 " />
+              <p className="text-sm font-medium">Food</p>
+            </div>
+
+            {/* Date */}
+            <div>
+              <p className="text-sm">May 17, 2023</p>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
---
+++ components/navigation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Navigation = () => {
+  return (
+    <header className="px-4 py-4">
+      <div className="flex items-center justify-between">
+        <nav>
+          <RxHamburgerMenu className="text-xl" />
+        </nav>
+
+        {/* User Information */}
+        <div className="flex items-center gap-2">
+          {/* Name */}
+          <p>Hi, John Doe</p>
+
+          {/* Profile picture */}
+          <div className="h-[40px] w-[40px] overflow-hidden rounded-full">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className="h-full w-full object-cover"
+              src="https://qph.cf2.quoracdn.net/main-qimg-134e3bf89fff27bf56bdbd04e7dbaedf.webp"
+              alt="AI generated Profile Image"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
