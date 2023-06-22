432b57ea2925bd239b7f328e1d2f0871d33371b9
Jericho Filllon <fillonjericho@gmail.com>
2023-06-22 00:40:22 -0700

    Cleared build cache, try #5

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { AuthUserContext } from "@/lib/authContext";
 import { ScheduleContext } from "@/lib/scheduleContext";
 
+// Deleted Build Cache, try #5.
 import { Navigation } from "@/components/Navigation.js";
 import { Expenses } from "@/components/Expenses/Expenses";
 import { PackingList } from "@/components/Packing/PackingList";
