89a3432072fd2a451b39c41c8cbc0eeee6464a1d
Jericho Filllon <fillonjericho@gmail.com>
2023-06-25 23:17:56 -0700

    Add styles for ViewSchedules

--- components/Schedule/ViewSchedules.js
+++ components/Schedule/ViewSchedules.js
@@ -7,7 +7,7 @@ import { ScheduleContext } from "@/lib/scheduleContext";
 import { dateFormatter } from "@/lib/utils";
 import { cn } from "@/lib/utils";
 
-import { Trash2 } from "lucide-react";
+import { Trash2, Map, CalendarClock, StickyNote } from "lucide-react";
 
 export const ViewSchedules = ({ schedule }) => {
   const { deleteSchedule } = useContext(ScheduleContext);
@@ -22,29 +22,50 @@ export const ViewSchedules = ({ schedule }) => {
 
   return (
     <>
-      <div className="mt-5 flex flex-col bg-slate-300 p-3">
+      <div className="mt-5 flex flex-col gap-3 rounded-xl border border-gray-300 px-4 py-2">
         {/* Title */}
-        <div className="my-3 flex w-full items-center justify-between">
-          <h2 className="font-bold">{schedule.title}</h2>
+        <div className="mt-4 flex w-full items-center justify-between">
+          <h2 className="font-semibold">{schedule?.title}</h2>
           <button
             type="button"
-            onClick={() => deleteScheduleHandler(schedule.id)}
+            onClick={() => deleteScheduleHandler(schedule?.id)}
           >
             <Trash2 className="h-4 w-4" />
           </button>
         </div>
 
-        <p>{schedule.selectedLocation.address}</p>
-
-        {/* Dates */}
-        <div className="flex gap-2">
-          <p>{dateFormatter(schedule.selectedDates.from)}</p>
-          <p> - </p>
-          <p>{dateFormatter(schedule.selectedDates.to)}</p>
+        {/* Location / Address */}
+        <div className="flex items-center gap-2">
+          <Map className="h-4 w-4 text-blue-500" />
+          <p className="text-sm font-light text-blue-500 underline underline-offset-4 ">
+            {schedule?.selectedLocation.address}
+          </p>
         </div>
 
-        {/* Text Area */}
-        <p>{schedule.notes}</p>
+        {/* Dates */}
+        <div className="flex items-center gap-2">
+          <CalendarClock className="h-4 w-4 text-gray-500" />
+          <p className="text-sm font-light text-gray-500">
+            {dateFormatter(schedule?.selectedDates.from)}
+          </p>
+          <p className="text-sm font-light text-gray-500"> - </p>
+          <p className="text-sm font-light text-gray-500">
+            {dateFormatter(schedule?.selectedDates.to)}
+          </p>
+        </div>
+
+        {/* Additional Notes */}
+        {schedule.notes && (
+          <div className="flex items-center gap-2 ">
+            <StickyNote className="h-4 w-4 text-gray-500" />
+            <p className="text-sm font-light text-gray-500">
+              {schedule?.notes}
+            </p>
+          </div>
+        )}
+
+        {/* View Map Button */}
+        <div></div>
       </div>
     </>
   );
